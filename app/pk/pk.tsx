190394f4b8b08772762e9bce254b4a7c0bac1ada
‪ ABDELWAHAB MOHAMED HAMED <abdelwahabmohammedbis@gmail.com>
2026-02-16 23:06:18 +0200

    Add files via upload

---
+++ app/pk/pk.tsx
@@ -0,0 +1,52 @@
+
+export default function PK(){ 
+  return (
+     
+    <section className="bk" id="bk">
+
+
+        <div className="about">
+            <h2>About Me</h2>
+            <p>I am a passionate software engineer with a strong interest in technology and software development. My journey in programming started at a young age and has continued through my academic pursuit of a Computer Science degree, along with participation in various real-world projects and tech events.
+
+I have solid experience in developing web applications, particularly in Back-End development using Node.js, and I also have a growing interest in data analytics. I’ve worked on practical projects for organizations, been selected for the ALX Software Engineering program, and participated in major tech events such as the Google Hackathon, among many others.
+
+What sets me apart is my ability to combine creative design with efficient software solutions, with a strong focus on continuous learning and staying up to date with the latest technologies.
+</p>
+
+        </div>
+
+
+        <div className="skills" id="skills">
+            <h2>Skills</h2>
+            <label htmlFor="">C programming</label>
+            <div className="C"></div>
+            <label htmlFor="">Python</label>
+            <div className="python"></div>
+            <label htmlFor="">C++</label>
+            <div className="C++"></div>
+            <label htmlFor="">Type Script</label>
+            <div className="typescript"></div>
+            <label htmlFor="">Java Script</label>
+            <div className="javascript"></div>
+            <label htmlFor="">Java</label>
+            <div className="java"></div>
+               <label htmlFor="">sqlite</label>
+            <div className="sqlite"></div>
+            <label htmlFor="">SQL SERVER</label>
+            <div className="sqlserver"></div>
+            <label htmlFor="">HTML</label>
+            <div className="html"></div>
+            <label htmlFor="css">CSS</label>
+            <div className="css"></div>
+            <label htmlFor="css">express</label>
+            <div className="express"></div>
+            <label htmlFor="css">Node Js </label>
+            <div className="nodejs"></div>
+        </div>    
+        
+
+        
+    </section>
+  )
+} 
